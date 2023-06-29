230abb96243284aad76ba4db2bf895d0eeb38304
Piotr Ryczek <piotrryczek@gmail.com>
2023-06-29 16:14:58 +0200

    Spaces & Rooms separated

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import MultiplayerPrivateRooms from "./MultiplayerPrivateRooms";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<MultiplayerPublicSpaces />);
+root.render(<MultiplayerPrivateRooms />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
